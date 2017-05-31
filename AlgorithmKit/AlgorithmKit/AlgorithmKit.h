d67167669e302595feabd93a80c6cf5d0a33e6ac
liujianjing <liujj@fenbi.com>
2017-05-31 17:12:06 +0800

    ADD: project init.

---
+++ AlgorithmKit/AlgorithmKit/AlgorithmKit.h
@@ -0,0 +1,19 @@
+//
+//  AlgorithmKit.h
+//  AlgorithmKit
+//
+//  Created by SwiftZimu on 2017/5/31.
+//  Copyright © 2017年 swiftlang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlgorithmKit.
+FOUNDATION_EXPORT double AlgorithmKitVersionNumber;
+
+//! Project version string for AlgorithmKit.
+FOUNDATION_EXPORT const unsigned char AlgorithmKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgorithmKit/PublicHeader.h>
+
+
